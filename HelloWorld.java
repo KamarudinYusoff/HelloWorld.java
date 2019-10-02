09271255b21b69d07730655e6a749137f30aa37e
KamarudinYusoff <55368410+KamarudinYusoff@users.noreply.github.com>
2019-10-02 10:18:50 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+    public static void main(String[] Kamarudin) {
+        System.out.println("A=  " + 3*6/2 );
+        System.out.println("B=  " + 3*4/2 );
+        System.out.println("C=  " + 4*3);
+        System.out.println("A+B+C= " + (9+6+12));
+
+
+    }
+}
